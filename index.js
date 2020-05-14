ff40f85b567a266db0b983f454c6b9f0409fedf2
thiago.xavier <thiago.xavier@unidas.com.br>
2020-05-13 22:11:06 -0300

    iniciando a conexão com mysql

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 //carregar a biblioteca do express
 var express = require('express');
+const dbMysql = require('mysql');
 //incializando o express
 var app = express();
+
+
+
+//Criar a conexão 
+var db = dbMysql.createConnection(
+  {
+    host: "db4free.net",
+    user: "automacaouninove",
+    password: "uninove10",
+    database: "lojacadastro"
+  }
+);
+
+//Iniciar a conexão
+db.connect(function (error) {
+  if (error) throw error; //expor os erros que ocorrem na conexão
+  console.log("Conectado com sucesso!");
+});
+
+//função para executar as queries
+function executarQuery(sql, response) {
+  db.query(sql, function (error, result, fields) {
+    if (error)
+      response.json(error);
+    else
+      response.json(result);
+  });
+}
+
+
+//Retornar usuários
+app.get('/usuarios', function (req, response) {
+  const sql = "select * from usuarios";
+  executarQuery(sql, response);
+});
+
 
 //endpoints ou endereços da web
 app.get('/', function (req, res) {
@@ -10,14 +47,14 @@ app.get('/', function (req, res) {
 
 
 //criar o novo endpoint
-app.get('/nomecliente', function(req, res){
-    let nome = {
-        "Thiago" : "Xavier"
-    }
-    res.json(nome);
+app.get('/nomecliente', function (req, res) {
+  let nome = {
+    "Thiago": "Xavier"
+  }
+  res.json(nome);
 });
 
 //a porta que será exposta
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3003, function () {
+  console.log('Example app listening on port 3003!');
 });
